60cc6c5608398a5385a16b0cd3627fd05832436a
balf0605 <sharvanphansikar@gmail.com>
2025-03-25 14:40:07 +0530

    Disable debug mode in Flask app for production readiness

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Ensures compatibility with Render
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
